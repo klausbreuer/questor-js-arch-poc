b93ca9f21288fe1cd62d0cbccb6a425103502737
klausbreuer <k.breuer.com@googlemail.com>
2011-12-21 18:10:59 +0100

    first simple renderer, can show a quiz station

--- questor-js-arch-poc/src/de/questor/poc/jsarch/QuestorJsArchPocActivity.java
+++ questor-js-arch-poc/src/de/questor/poc/jsarch/QuestorJsArchPocActivity.java
@@ -6,6 +6,10 @@ import android.util.Log;
 import android.webkit.WebView;
 
 public class QuestorJsArchPocActivity extends Activity {
+
+	Renderer mRenderer ;
+	
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,12 +22,21 @@ public class QuestorJsArchPocActivity extends Activity {
 		wv.addJavascriptInterface(new Questor(), "questor");
 
 		wv.loadUrl("file:///android_asset/main.html");
+		
+		mRenderer = new Renderer(this);
+		
 	}
 
+
 	/** Questor class which is accessible from server generated Javascript code */
-	static class Questor {
+	public class Questor {
 		public void becomeRenderer() {
 			Log.i("questor", "renderer");
+			
+			//mRenderer.onMessage("alert('buh!');showToast('hihi');");
+			//mRenderer.onMessage("var q = new Renderer.QuizStation ('wie hiess die tarent frueher, als alles noch viel frueher war?'); q.onSubmit('cic');");
+			mRenderer.onMessage("var q = new Renderer.QuizStation ('wie hiess die tarent frueher, als alles noch viel frueher war?');");
+			
 		}
 
 		public void becomeSimulator() {
