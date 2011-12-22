1e18f0b7a19cb43124d74b8775d00b3a2671868f
klausbreuer <k.breuer.com@googlemail.com>
2011-12-22 15:19:30 +0100

    gerade schon wieder vergessen, die files für das commit zu markiern :-)

---
+++ questor-js-arch-poc/src/de/questor/poc/jsarch/HtmlActivity.java
@@ -0,0 +1,45 @@
+package de.questor.poc.jsarch;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class HtmlActivity extends Activity {
+
+	private WebView mWebView;
+	private String mContent;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		// Get the content from the intent:
+		Intent i = getIntent();
+		mContent = (String) i.getSerializableExtra("content");
+		
+		mWebView = new WebView(this);
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.setWebChromeClient(new WebChromeClient());
+		mWebView.loadData(mContent, "text/html", null);
+		
+		setContentView(mWebView);
+		
+		Log.i("klaus", mContent);
+
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+}
